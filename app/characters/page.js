da3c311b3b7a107f416812a0730d334618de5d93
walton <zjxcdut@gmail.com>
2024-11-01 15:09:16 -0300

    Finished characters page

--- app/characters/page.js
+++ app/characters/page.js
@@ -1,10 +1,35 @@
 
-
-export default function Characters() {
-  return (
-    <>
-      <h2>Characters page</h2>
-    </>
+// function to fetch characters from API
+async function getCharacterss() {
+    let apiUrl = 'https://hp-api.onrender.com/api/characters';
+    let characters = [];
     
-  );
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    characters = characters.concat(data);
+    
+    return characters;
+}
+
+// Characters component
+export default async function Characters() {
+    const characters = await getCharacterss();
+    
+    return (
+        <>
+            <h2>Characters page</h2>
+            {characters.length > 0 && characters.map((character) => ( 
+            <div className="row mb-3" key={character.id}>
+                <div className="col-lg-2">
+                    <img src={character.image} alt={character.name} className="img-fluid" />
+                </div>
+                <div className="col-lg-10">
+                    <h3>{character.name}</h3>
+                    <blockquote>"{character.species}"</blockquote>
+                    <p>{character.house}</p>
+                </div>
+            </div>
+            ))} 
+        </>
+    );
 }
